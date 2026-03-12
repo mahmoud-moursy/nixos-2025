896ee338381e8a2e9cea32425411ec18c6595019
Mahmoud Moursy <bageliq@protonmail.com>
2026-03-13 03:55:07 +0400

    Try and fix IPv6 once and for all attempt 3

--- hardware/realtek-rtl8125.nix
+++ hardware/realtek-rtl8125.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }: {
   boot.blacklistedKernelModules = [ "r8169" ]; # Causes breakage for RTL 8125 LAN specifically
 
-  boot.extraModulePackages = [ pkgs.r8125 ];
+  boot.extraModulePackages = [ pkgs.linuxPackages.r8125 ];
   boot.kernelModules = [ "r8125" ];
 }
