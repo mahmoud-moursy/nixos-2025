73bb3dbc405a73bad3351910417f93395446b460
Mahmoud Moursy <bageliq@protonmail.com>
2026-07-03 01:17:07 +0400

    Fix config error

--- hardware/alienware/nvidia.nix
+++ hardware/alienware/nvidia.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, config, ... }: {
   # The open source driver does not support Pascal GPUs.
   hardware.nvidia.open = false;
   hardware.nvidia.package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.legacy_580;
