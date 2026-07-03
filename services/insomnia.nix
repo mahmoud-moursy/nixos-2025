0f14d1ae227f52bb6dd3c24330c74e5beabe1780
Mahmoud Moursy <bageliq@protonmail.com>
2026-07-03 04:39:18 +0400

    Fix free CPU heating from lid closing

--- services/insomnia.nix
+++ services/insomnia.nix
@@ -4,4 +4,13 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+
+  services.logind.settings.Login.HandleLidSwitch = "ignore";
+  services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
+  services.logind.settings.Login.HandleLidSwitchDocked = "ignore";
+
+  services.logind.settings.Login.HandlePowerKey = "ignore";
+  services.logind.settings.Login.HandleRebootKey = "ignore";
+  services.logind.settings.Login.HandleSuspendKey = "ignore";
+  services.logind.settings.Login.HandleHibernateKey = "ignore";
 }
